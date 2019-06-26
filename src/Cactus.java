185371e5acf055a09c8abf5450e63b9ec4f41bf6
llalma <lhulsmanbenson@gmail.com>
2019-06-26 20:38:33 +1000

    Finished game

--- src/Cactus.java
+++ src/Cactus.java
@@ -1,15 +1,25 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 
 public class Cactus extends Obstacle{
+    Image scaled_img;
 
     public Cactus(int x){
         super(x,360, 30,60);
+
+        try {
+            Image img = ImageIO.read(getClass().getResource("images/cactus.png"));
+            scaled_img = img.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+        }catch (Exception E){
+
+        }
     }
 
     public void draw(Graphics g){
         g.setColor(Color.cyan);
         this.x -= game_speed;
-        g.fillRect(x,y,width,height);
+//        g.fillRect(x,y,width,height);
+        g.drawImage(scaled_img,x,y,width,height,null);
     }
 
 }
--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 
 public class Dinosaur {
@@ -9,6 +10,7 @@ public class Dinosaur {
 
     final int screen_height = 400;
     final double Ground_thicness = 0.1;
+    Image scaled_img;
 
 
     public Dinosaur(){
@@ -16,11 +18,19 @@ public class Dinosaur {
         dino_height = 60;
 
         y = screen_height - (screen_height*Ground_thicness)-dino_height;
+
+        try {
+            Image img = ImageIO.read(getClass().getResource("images/dino.jpg"));
+            scaled_img = img.getScaledInstance(50, 50, java.awt.Image.SCALE_SMOOTH);
+        }catch (Exception E){
+
+        }
     }
 
     public void draw(Graphics g){
         g.setColor(Color.BLACK);
-        g.fillRect(x,(int)y,(int)dino_width,(int)dino_height);
+//        g.fillRect(x,(int)y,(int)dino_width,(int)dino_height);
+        g.drawImage(scaled_img,x,(int)y,(int)dino_width,(int)dino_height,null);
     }
 
     public void jump(){
